22aacd797913212b4789ea89fbcaa49f788d6b91
gjohnest <gjohnest@gmail.com>
2025-07-12 16:05:38 +0200

    Initial commit

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,50 +1,60 @@
-import React,{useEffect,useState} from 'react'
-import YouTube from 'react-youtube'
-import './RowPost.css'
-import {imageUrl,API_KEY} from '../../constants/constants'
-import axios from '../../axios'
-import { Axios } from 'axios'
+import React, { useEffect, useState } from 'react';
+import YouTube from 'react-youtube';
+import './RowPost.css';
+import { imageUrl, API_KEY } from '../../constants/constants';
+import axios from '../../axios';
 
-function RowPost(props) {    
-  const [movies,setMovies]=useState([])
-  const [urlId,seturlId]=useState('')
+function RowPost(props) {
+  const [movies, setMovies] = useState([]);
+  const [videoData, setVideoData] = useState(null);
+
   useEffect(() => {
-  axios.get(props.url).then(Response=>{
-    setMovies(Response.data.results)
+    axios.get(props.url).then(response => {
+      setMovies(response.data.results);
+    }).catch(error => {
+      console.error('Failed to fetch movies', error);
+    });
+  }, [props.url]);
 
-  })
-  },[])
-  
-    const opts = {
-      height: '390',
-      width: '100%  ',
-      playerVars: {
-        // https://developers.google.com/youtube/player_parameters
-        autoplay: 1,
-      },
-    };
-    const handleMovie = (id)=>{
-      axios.get(`movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
-        if(response.data.results.length!==0){seturlId(response.data.results[0]);}
-        
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
+  const handleMovie = id => {
+    axios.get(`movie/${id}/videos?api_key=${API_KEY}&language=en-US`)
+      .then(response => {
+        if (response.data.results.length > 0) {
+          setVideoData(response.data.results[0]);
+        } else {
+          console.log('No trailer found');
+        }
       })
+      .catch(error => {
+        console.error('Failed to fetch video', error);
+      });
+  };
 
-    }
-    return (
-    <div
-    className='row'>
-        <h2>{props.title}</h2>
-        <div className='posters'>
-          {movies.map((obj)=>
-            <img onClick={()=>handleMovie(obj.id
-            )} className={props.isSmall ? 'smallPoster' :'poster'} alt='poster' src={`${imageUrl+obj.backdrop_path}`}></img>
-
-        )}
-        </div>
-      {urlId && <YouTube opts={opts} videoId= {urlId.key} />}
+  return (
+    <div className="row">
+      <h2>{props.title}</h2>
+      <div className="posters">
+        {movies.map(movie => (
+          <img
+            key={movie.id}
+            onClick={() => handleMovie(movie.id)}
+            className={props.isSmall ? 'smallPoster' : 'poster'}
+            alt={movie.title || movie.name || 'Movie poster'}
+            src={`${imageUrl + movie.backdrop_path}`}
+          />
+        ))}
+      </div>
+      {videoData && <YouTube opts={opts} videoId={videoData.key} />}
     </div>
-  )
+  );
 }
 
-export default RowPost
+export default RowPost;
--- src/Components/navbar/NavBar.js
+++ src/Components/navbar/NavBar.js
@@ -5,7 +5,7 @@ function NavBar() {
   return (
     <div className='navbar'>
       <img className="logo" src="https://www.freepnglogos.com/uploads/netflix-logo-symbol-png-1.png" alt="Netflix logo" />
-    <img className='avatar' src="https://cdn-icons-png.flaticon.com/128/9131/9131529.png" />
+    <img className='avatar' src="https://cdn-icons-png.flaticon.com/128/9131/9131529.png" alt="Netflix avatar"/>
     </div>
 
   );
